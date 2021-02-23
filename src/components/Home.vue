49d3563cdb48d93b37616d0f263f44bbe6f9e131
unknown <874039270@qq.com>
2021-02-23 22:03:33 +0800

    登录功能完成

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <html>
+        <el-button type="info" @click = "logout">退出</el-button>
+    </html>
+</template>
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
